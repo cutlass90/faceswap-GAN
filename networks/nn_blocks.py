4117c70bd0f175fa1c4c395e580c59a233fd1a04
Nazar Shmatko <cutlass90@mail.ru>
2019-12-04 21:21:56 +0200

    Update nn_blocks.py

--- networks/nn_blocks.py
+++ networks/nn_blocks.py
@@ -1,4 +1,5 @@
 from keras.layers import *
+from keras import regularizers
 from keras.layers.advanced_activations import LeakyReLU
 from .instance_normalization import InstanceNormalization
 from .GroupNormalization import GroupNormalization
